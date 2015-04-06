887b4c6912b6a7646c561f9ed92c36739a48d380
Brian Franco <mastastealth+git@gmail.com>
2015-04-06 17:46:20 -0400

    Init

---
+++ client/app.js
@@ -0,0 +1,16 @@
+var players = [
+	"Andy",
+	"Nguyen",
+	"Alex",
+	"Mastastealth",
+	"Deadbones",
+	"Nasal",
+	"Youthful",
+	"CORL"
+];
+
+Template.bracket.helpers({
+	"columns" : function() {
+		return [0,1,2,3];
+	}
+});
