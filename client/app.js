6a0bd0b3bdf21b158452eaef8603b56c8b821242
Brian Franco <mastastealth+git@gmail.com>
2015-04-09 01:48:59 -0400

    Everything is better.
    More stuff.

--- client/app.js
+++ client/app.js
@@ -1,40 +1,95 @@
-Meteor.subscribe('Matches');
-Matchups = new Mongo.Collection(null);
-
+// Initialize Variables
 var players = [];
-var brackets = [];
-var wins = 2;
+var bestOf = 2;
+Session.set('champion',false);
+
+// Subscriptions
+Meteor.subscribe('Matches');
+Matchups = new Mongo.Collection(null); // Local
 
 Meteor.subscribe('Players', function() {
+	// TODO: Replace with submission find or something
 	players = Players.find().fetch();
 	players = shuffle(players);
 
-	// Pair up
-	for (var i = 0; i < players.length; i += 2) {
-		// players[i].score = 0;
-		// players[i+1].score = 0;
-	    brackets.push([players[i], players[i+1]]);
-	}
+	var playerCount = players.length;
+	var t = playerCount/2;
+	var c = 1;
+	var parent = false;
 
-	//Session.set('brackets',brackets);
-	//console.log(players,brackets);
+	// Add ALL the matchups in this bracket
+	for (var i=0; i<playerCount-1; i+=1) {
+		// Reset tier counter
+		if (c>t) {
+			t = t/2;
+			c = 1;
+		}
+		// Update player parents for 2nd column+
+		//if (t !== playerCount/2) {
+			//parent = Matchups.find().fetch()[(playerCount/2)+c]._id;
+		//}
 
-	// Load initial matchups into DB
-	for (var j=0; j<brackets.length; j+=1) {
 		Matchups.insert({
 			"players" : [
 				{
-					"name": brackets[j][0].name,
+					"name": "",
 					"score": 0,
-					"id" : 0
+					"parent": parent,
+					"winner" : 0
 				},
 				{
-					"name": brackets[j][1].name,
+					"name": "",
 					"score": 0,
-					"id" : 1
+					"parent": parent,
+					"winner" : 0
 				}
 			],
-			"winner" : false
+			"winner" : "",
+			"tier" : t,
+			"bestof" : bestOf
+		});
+
+		c++;
+	}
+
+	// Update parents of 2nd+ round matchups
+	// =====================================
+	// TODO: Make this properly dynamic
+	var areParents = Matchups.find({tier:playerCount/2}).fetch();
+
+	var round2 = Matchups.find({tier: 2 }).fetch();
+	var round3 = Matchups.find({tier: 1 }).fetch();
+
+	for (var k=0; k<=2; k+=2) {
+		Matchups.update({ _id: round2[k/2]._id },{
+			$set: { 
+				"players.0.parent": areParents[k]._id,
+				"players.1.parent": areParents[k+1]._id,
+			}
+		});
+	}
+
+	Matchups.update({ _id: round3[0]._id },{
+		$set: { 
+			"players.0.parent": round2[0]._id,
+			"players.1.parent": round2[1]._id,
+		}
+	});
+
+	// -----------------------------------------
+	//   TEMP: Create initial matchups randomly
+	// -----------------------------------------
+	var m = Matchups.find({tier:playerCount/2}).fetch();
+	var p = players;
+
+	for (var j=0; j<m.length; j+=1) {
+		//console.log(m[j]._id);
+
+		Matchups.update({ _id: m[j]._id },{
+			$set: { 
+				"players.0.name": p.pop().name,
+				"players.1.name": p.pop().name
+			}
 		});
 	}
 });
@@ -59,213 +114,168 @@ function shuffle(array) {
 	return array;
 }
 
+// Generate columns based off number of players
+// participating in tournament
 Template.bracket.helpers({
 	"columns" : function() {
-		var brackets = Matchups.find().fetch();
-		var blen = brackets.length*2;
+		var columns = [];
+		// TODO: Replace with just players playing this time
+		var x = Math.log2( Players.find().fetch().length ) + 1;
 
-		// First Column
-		// Set as first column, set player count to full
-		if (brackets[0]) {
-			brackets[0].isFirst = true;
-			brackets[0].count = blen;
+		for (var i=0; i<x; i+=1) {
+			if (i === x-1) {
+				columns.push({ 
+					matches : 0
+				});
+			} else if (i===0) {
+				columns.push({ 
+					matches : Players.find().fetch().length/2
+				});
+			} else {
+				columns.push({ 
+					matches : Players.find().fetch().length/(2*(i*2))
+				});
+			}
 		}
 
-		// TODO: Need to figure out a way to set the
-		// index of the column in here...
-		if (brackets[1]) brackets[1].count = blen/2;
-		if (brackets[2]) brackets[2].count = blen/4;
-		if (brackets[3]) brackets[3].count = blen/8;
-		if (brackets[4]) brackets[4].count = blen/16;
-
-		// Last Column
-		// Set as last column (for champion class)
-		if (brackets[brackets.length - 1]) {
-			brackets[brackets.length - 1].isLast = true;
-		}
-		// Player Count
-
-		// Finally
-		return brackets;
+		return columns;
 	}
 });
 
 Template.column.helpers({
-	"id" : function() {
-		return this._id;
-	},
-	"count" : function() {
-		return this.count;
-	},
 	"isChamp" : function() {
-		if (this.isLast) {
-			return "champion";
-		} else {
-			return false;
+		if (this.matches === 0) {
+			return "champion"
 		}
 	},
 	"matchup" : function() {
-		if (this.isFirst) {
-			// Spit it all out
-			return Matchups.find().fetch();
-		} else {
-			// Correct number of matchups, blank for now
-			var x = [];
-
-			// This is the pair of players per matchup
-			for (var i = 0; i < this.count/2; i += 1) {
-			    x.push({
-			    	players : [{
-			    		name:'',id:0
-				    },{
-				    	name:'',id:1
-				    }]
-			    });
-			}
-
-			// Get some real info
-			var y = Matchups.find({
-				// TODO: This somehow needs to reflect a $gt that matches
-				// the column properly as to show subsequent winners
-				players : { 
-					$elemMatch : { score: { $gt: (players.length/this.count)-1 } } 
-				} 
-			}).fetch();
-
-			// Replace x info with actual data
-			for (var j = 0; j < y.length; j += 1) {
-			    x[Math.floor(j/2)].players[j%2] = y[j].players[ y[j].winner ];
-			}
-
-			return x;
-		}
+		return Matchups.find({ tier: this.matches });
 	}
 });
 
 Template.match.helpers({
-	"count" : function() {
-		return this.count
-	},
-	"pair" : function() {
-		return this.players;
-	},
 	"id" : function() {
 		return this._id;
+	},
+	"pair" : function() {
+		var p = this.players;
+		//p.winner = this.winner; 
+		return p;
+	},
+	"winner" : function() {
+		return this.winner;
 	}
 });
 
 Template.champ.helpers({
-	"name" : function() {
-		var max = Players.find().fetch().length;
-		var win = 0;
-
-		if (max===4) win = 3;
-		if (max===8) win = 4;
-		if (max===16) win = 5;
-
-		var y = Matchups.find({
-			players : { 
-				$elemMatch : { score: { $gt: (wins*(win-1))-1 } } 
-			} 
-		}).fetch();
-
-		if (y[0]) {
-			if (y[0].players[0].score >= wins*(win-1)) {
-				return y[0].players[0].name
-			} else { return y[0].players[1].name }
-		}
+	"name" : function()  {
+		return Session.get('champion');
+	},
+	"winner" : function() {
+		if (Session.get('champion')) return "animated rubberBand infinite"
 	}
-})
+});
 
 Template.player.helpers({
 	"id" : function() {
-		return this.id;
+		return this.parent;
 	},
 	"name" : function() {
 		return this.name;
 	},
 	"score" : function() {
-		// Max number of players in the game
-		var max = Matchups.find().fetch().length*2;
-		var n = 0;
-		if (Template.parentData(2)) n = Template.parentData(2).count;
-
-		// Max out first column at "wins" number
-		if (this.score>=wins && n === max) {
-			return wins;
-		} 
-		// Max out other columns at "wins" number as well
-		// or show remainder
-		else if (this.score>=wins && n != max) {
-			if (this.score>=wins*(max/n)) {
-				return wins;
-			} else {
-				return this.score % wins;
-			}
-		} 
-		// Otherwise just add like normal
-		else {
-			return this.score;
+		return this.score;
+	},
+	"status" : function() {
+		return this.winner;
+	},
+	"isWinnerConfirm" : function() {
+		if (this.score>=bestOf) {
+			return true;
+		} else {
+			return false;
 		}
+	},
+	"isWinner" : function() {
+		if (this.winner === 1) return "animated tada";
+	},
+	"isLoser" : function() {
+		if (this.winner === -1) return "loser";
 	}
-});
+})
 
 Template.player.events({
 	"click .add" : function(e) {
-		if (true) {
-			var par = e.target.parentNode.parentNode.parentNode.getAttribute('id');
-			
-			Matchups.update({
-				"_id" : par,
-				"players.name" : this.name 
-			}, { 
-				$set: { 
-					"players.$": { 
-						name: this.name,
-						id: this.id,
-						score: this.score+1 
-					} 
-				} 
-			});
+		if (this.score>=bestOf) return false;
 
-			// If they are currently a winner
-			if (this.score+1>1) {
-				Matchups.update( 
-					par, { 
-						$set: { "winner": this.id } 
-				});
-			} else {
-				Matchups.update( 
-					par, { 
-						$set: { "winner": false } 
-				});
+		var parent = (!this.id) ? Template.parentData(1)._id : this.id;
+		console.log(parent);
+
+		Matchups.update({ _id: parent, "players.name":this.name },{
+			$inc: { 
+				"players.$.score": 1
 			}
-		}
+		});
 	},
 	"click .sub" : function(e) {
-		if (this.score>0)  {
-			var par = e.target.parentNode.parentNode.parentNode.getAttribute('id');
-			
-			Matchups.update({
-				"_id" : par,
-				"players.name" : this.name 
-			}, { 
-				$set: { 
-					"players.$": { 
-						name: this.name,
-						id: this.id,
-						score: this.score-1 
-					} 
-				} 
-			});
+		if (this.score<=0) return false;
 
-			// Remove if was winner
-			if (this.score-1<2) {
-				Matchups.update( 
-					par, { 
-						$set: { "winner": false } 
+		var parent = (!this.id) ? Template.parentData(1)._id : this.id;
+
+		Matchups.update({ _id: parent, "players.name":this.name },{
+			$inc: { 
+				"players.$.score": -1
+			}
+		});
+
+		// Mark both players as normal and remove any winners
+		Matchups.update({ _id: Template.parentData(1)._id },{
+			$set: { 
+				"winner" : "",
+				"players.0.winner": 0,
+				"players.1.winner": 0
+			}
+		});
+
+	},
+	"click .win" : function(e) {
+		var parent = Template.parentData(1)._id
+		console.log("Setting winner on "+parent);
+		// Set match winner
+		Matchups.update({ _id: parent },{
+			$set: { 
+				"winner": this.name,
+			}
+		});
+		// Mark player as winner
+		Matchups.update({ _id: parent, "players.name":this.name },{
+			$set: { 
+				"players.$.winner": 1
+			}
+		});
+		// Mark other player as loser
+		Matchups.update({ _id: parent, "players.winner":0 },{
+			$set: { 
+				"players.$.winner": -1
+			}
+		});
+		// Set winner as player of next round
+		var pCheck = Matchups.find({ "players.parent" : parent  }).fetch();
+
+		if (!pCheck[0]) {
+			// Champion
+			Session.set('champion',this.name);
+		} else {
+			// Not Champion yet
+			if (pCheck[0].players[0].parent === parent) {
+				Matchups.update({ "players.parent" : parent  },{
+					$set: { "players.0.name": this.name }
+				});
+			} else if (pCheck[0].players[1].parent === parent) {
+				Matchups.update({ "players.parent" : parent  },{
+					$set: { "players.1.name": this.name }
 				});
 			}
 		}
-	},
+	}
 });
